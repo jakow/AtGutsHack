1ef7d4797e9e7810aa07d91ff157cf0c382b7ce7
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2016-10-15 19:09:15 +0100

    more classes

---
+++ js/Agent.js
@@ -0,0 +1,18 @@
+// var Vector2 = require('./Vector2');
+
+class Agent {
+
+	constructor(position = new Vector2(0,0),
+		speed = new Vector2(0,0),
+		radius = 0.01,
+		flockingRadius = 1) {
+		this.position = position;
+		this.speed = speed;
+		this.radius = 0.01;
+		this.flockingRadius = flockingRadius;
+	}
+	
+
+
+
+}
