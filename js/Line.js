8a822797a7fa4464e4ce697e5543c17290cf4d94
Pawel Safuryn <Pawel Safuryn>
2016-10-16 00:34:31 +0100

    Min distance to line

--- js/Line.js
+++ js/Line.js
@@ -14,6 +14,19 @@ constructor(start = new Vector2(0,0), end = new Vector2(0,0)){
 	minDistance(point) {
 		var len = this.length();
 		var len_sq = len*len;
+		// t defining parametric eq
+		var start = this.start;
+		var end = this.end;
+		var t = point.sub(start).dot(end.sub(start))/len_sq;
+		if (t < 0) return point.to(start).mag();
+
+		else if (t > 1) return point.to(end).mag();
+
+		else {
+			var projection = start + t*end.sub(start);
+			return point.to(projection).mag();
+		}
+	
 	}
 }
 
