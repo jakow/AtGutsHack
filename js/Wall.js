c6c93f28081076c2fd8452aedb45be13bfee5302
Filip Kaklin <filip.kaklin@gmail.com>
2016-10-15 23:21:35 +0100

    Added Wall.js to index head

--- js/Wall.js
+++ js/Wall.js
@@ -3,13 +3,15 @@ class Wall {
   constructor(lines = new Line(new Vector2(0,0), new Vector2(1,1))) {
     if (Array.isArray(lines))
     	this.lines = lines;
-    else 
+    else
     	this.lines = [lines];
   }
 
   draw() {
   	lines.forEach(function(line) {
-  		// draw line
+      ctx.moveTo(line.start.x, line.start.y)
+      ctx.moveTo(line.end.x, line.end.y);
+      ctx.stroke();
   	});
   }
 
