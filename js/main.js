1faa3b5f51da11c492d91dbd1e14ea8086b6f122
Filip Kaklin <filip.kaklin@gmail.com>
2016-10-16 12:50:54 +0100

    Added canvas clearing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-
-
 // Add checking if agant was spawned on the LINE/WALL
 // Create new agents at postitions specified by positions array
 
 var timestep = 0.03;
 function init() {
+c = document.getElementById('canvas2');
 ctx = document.getElementById('canvas2').getContext('2d');
-
 
 var agents = generateAgents(generatePositions(100, 0, 400, 0, 400));
 agents.forEach(agent => agent.draw());
 
 function recomputePositions() {
- 	
 
  	agents = agents.map(function(agent, idx, agents) {
 
@@ -20,24 +17,14 @@ function recomputePositions() {
 		return agent.setPosition(agent.computeVelocity(others));
  	});
  	return agents;
- 	
-
- 	// console.log('recomputing');
-
 }
-
 
 recomputePositions();
 
 function frame() {
+  ctx.clearRect(0,0,c.width,c.height);
 	recomputePositions().map(agent => agent.draw());
 }
 var tick = setInterval(frame, timestep*1000);
-
 // setTimeout(tick => clearInterval(tick), 10000);
-}
-
-window.onload = init;
-
-
-
+} window.onload = init;
